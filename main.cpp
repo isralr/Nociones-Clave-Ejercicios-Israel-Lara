0cf4af47864e343db20d41fd1c07ee4029dc6b44
isralr <ilaramac@myuax.com>
2024-10-10 18:40:18 +0200

    Añadido suma

--- main.cpp
+++ main.cpp
@@ -3,9 +3,15 @@
 //
 
 #include "main.h"
-
 #include <iostream>
+
+int suma(int a, int b) {
+  return a + b;
+}
+
 int main() {
   std::cout << "Hola" << std::endl;
+  std::cout << "La suma es: " << suma( 5 , 3) << std::endl;
+
   return 0;
 }
